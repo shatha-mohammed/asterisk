0c07d5f1aae10d7ccc9c87fa21b7815a77118774
ahmedtika74 <tekaliker@gmail.com>
2026-04-26 16:52:37 +0300

    fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 import { Routes, Route } from "react-router-dom";
-import Header from "./layout/Header";
+import MainLayout from "./layout/MainLayout";
 import Home from "./pages/Home";
 import Pricing from "./pages/Pricing";
 import About from "./pages/About";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 export default function App() {
   return (
-    <div className="px-4 lg:px-24">
-      <Header />
-      <div className="h-16"></div>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/pricing" element={<Pricing />}></Route>
-        <Route path="/about" element={<About />}></Route>
-      </Routes>
-    </div>
+    // <div className="px-4 lg:px-24">
+    //   <Header />
+    //   <div className="h-16"></div>
+    //   <Routes>
+    //     <Route path="/" element={<Home />}></Route>
+    //     <Route path="/pricing" element={<Pricing />}></Route>
+    //     <Route path="/about" element={<About />}></Route>
+    //   </Routes>
+    // </div>
+
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/about" element={<About />} />
+      </Route>
+
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   );
 }

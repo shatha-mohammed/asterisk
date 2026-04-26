5b2bc36d9e19bad9bcf5215f6b26b15625b61d91
ahmedtika74 <tekaliker@gmail.com>
2026-04-26 05:47:03 +0300

    fix classes names

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,18 +1,12 @@
 export default function AppLayout({ children }) {
   return (
-    <div className="flex bg-slate-950 text-white min-h-screen">
-      <div className="w-[240px] bg-slate-900 p-4">
-        Sidebar
-      </div>
+    <div className="flex min-h-screen bg-slate-950 text-white">
+      <div className="w-60 bg-slate-900 p-4">Sidebar</div>
 
       <div className="flex-1">
-        <div className="h-[60px] bg-slate-900 p-4">
-          Navbar
-        </div>
+        <div className="h-15 bg-slate-900 p-4">Navbar</div>
 
-        <div className="p-6">
-          {children}
-        </div>
+        <div className="p-6">{children}</div>
       </div>
     </div>
   );

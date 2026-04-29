a92277dd64287ac8427cdbbf0d66fd687fb7a250
ahmedtika74 <tekaliker@gmail.com>
2026-04-29 17:25:00 +0300

    create PageHeader component for pages in dashboard

---
+++ src/Components/ui/PageHeader.jsx
@@ -0,0 +1,24 @@
+export default function PageHeader({ title, subtitle, action }) {
+  return (
+    <div className="mb-10 flex flex-col md:flex-row md:items-center md:justify-between">
+      <div>
+        {/* Main page title*/}
+        <h1 className="text-3xl font-black tracking-tight text-indigo-900 capitalize lg:text-5xl">
+          {title}
+        </h1>
+
+        {/* Subtitle */}
+        {subtitle && (
+          <p className="text-muted mt-2 text-sm font-bold tracking-[0.2em] uppercase opacity-60">
+            {subtitle}
+          </p>
+        )}
+      </div>
+
+      {/* Action element */}
+      {action && (
+        <div className="mt-8 self-end md:self-auto lg:mt-0">{action}</div>
+      )}
+    </div>
+  );
+}
